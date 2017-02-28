d43e5b8d9676941112c3601b4125239e60cbd5d8
roby <roberto.casadei90@gmail.com>
2017-02-28 14:21:18 +0100

    Little update

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import model.Person;
+import utils.Logger;
+
 /**
  * @author Roberto Casadei
  */
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello world");
+        Logger l = new Logger();
+        Person p = new Person("Bob");
+        l.logPrintString("Hello ");
     }
 }
---
+++ src/utils/Logger.java
@@ -0,0 +1,17 @@
+package utils;
+
+/**
+ * @author Roberto Casadei
+ */
+
+public class Logger {
+    public static void logPrintString(String s){
+        System.out.format("[Print] %s\n",s);
+    }
+
+    public static void logPrintStrings(String[] array){
+        for (String s: array){
+            System.out.format("[Print] %s\n",s);
+        }
+    }
+}
